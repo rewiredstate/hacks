391d0725a75eb76d3099fd69a3a87ae5db029c40
Jordan Hatch <jordan@jordanh.net>
2011-11-20 23:49:09 +0000

    Properly sort winners

--- app/models/award.rb
+++ app/models/award.rb
@@ -1,4 +1,6 @@
 class Award < ActiveRecord::Base
   belongs_to :project
   belongs_to :award_category
+  
+  default_scope order('award_categories.format DESC, award_categories.level ASC')
 end                       
