eb251808ed29f55cc5baaca5fbe7d5759cb6c7f1
Jordan Hatch <jordan@jordanh.net>
2012-01-30 22:58:23 +0000

    Attempt at fix

--- app/models/award.rb
+++ app/models/award.rb
@@ -2,5 +2,5 @@ class Award < ActiveRecord::Base
   belongs_to :project
   belongs_to :award_category
   
-  default_scope order('award_categories.format DESC, award_categories.level ASC')
+  default_scope order('`award_categories`.format DESC, `award_categories`.level ASC')
 end                       
