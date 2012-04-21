c85807b8a2f4ab4fd94cb319bcad7a0304d385dd
Jordan Hatch <jordan@jordanh.net>
2012-04-22 00:45:43 +0100

    Added events index listing

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,24 +1,24 @@
 class EventsController < ApplicationController
-  before_filter :find_event, :except => :index  
-                      
+  before_filter :find_event, :except => :index
+
   def index
-    redirect_to event_url(Event.first)
+    @events = Event.all
   end
-  
+
   def show
     respond_to do |format|
       format.html { # show.html.erb
-        }                          
+        }
       format.json { # show.json.rabl
-        }                    
+        }
       format.csv { render :csv => @event.projects }
     end
-  end                      
-  
+  end
+
   private
     def find_event
-      @event = Event.find_by_slug(params[:id]) || not_found     
+      @event = Event.find_by_slug(params[:id]) || not_found
       breadcrumbs.add @event.title, event_url(@event)
-    end        
+    end
 
 end
