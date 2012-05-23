149efe709304b12739498096ea7ceda8ae37a874
Jordan Hatch <jordan@jordanh.net>
2012-05-23 08:21:34 +0100

    Reset secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Projects::Application.config.secret_token = 'fbd5114be24ff1ea4ff5f18197c5a2699d40b3c8619287f2cf4380921e202165c03a575e8cc74bf9359846946287d18d2a4b3da4c5d40ecb93ed56837f571428'
+Projects::Application.config.secret_token = ENV['SECRET_TOKEN'] || "an_insecure_secret_token_key_which_should_not_be_used_in_production"
