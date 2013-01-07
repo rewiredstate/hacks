6942c0ee2376e515587b9c617b0fc302f551372c
Adam McGreggor <adam@amyl.org.uk>
2013-01-07 19:50:23 +0000

    sometimes <http://hacks.rewiredstate.org/events/wthack-2012/medic-monitoring-early-detection-for-intensive-care> we have teams of 8

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,7 +1,7 @@
 module ProjectsHelper
   def twitter_links_for(project)
     output = []
-    project.twitter.split(/, ?/).take(4).each do |t| 
+    project.twitter.split(/, ?/).take(8).each do |t| 
       clean_user = sanitize t.strip.sub('@','')
       output << link_to("@#{clean_user}", "http://twitter.com/#{clean_user}") 
     end
