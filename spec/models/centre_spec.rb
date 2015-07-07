5d41361d6e6194cb45d668fd81392f13a4f9a804
Jordan Hatch <jordan@jordanh.net>
2015-07-07 12:49:13 +0100

    Update ProjectsHelper spec with new syntax

--- spec/models/centre_spec.rb
+++ spec/models/centre_spec.rb
@@ -1,44 +1,46 @@
 require 'rails_helper'
 
-RSpec.describe Centre do
+RSpec.describe Centre, type: :model do
+
+  let(:event) { create(:event_with_centres) }
 
   describe "creating a centre" do
-    before do
-      @event = FactoryGirl.create(:event, :use_centres => true)
-      @valid_attributes = {
-        :name => "London",
-        :slug => "centre-slug"
-      }
-    end
+    let(:attributes) {
+      attributes_for(:centre)
+    }
 
     context "given valid attributes" do
       it "can create a centre" do
-        @event.centres.create!(@valid_attributes)
+        event.centres.create!(attributes)
       end
 
       it "can set a slug if none exists" do
-        @centre = @event.centres.create!( @valid_attributes.merge({:slug => nil}) )
-        @centre.slug.should == "london"
+        centre = event.centres.create!( attributes.merge(slug: nil) )
+
+        expect(centre.slug).to eq(attributes[:name].parameterize)
       end
 
       it "doesn't generate a slug if one is present" do
-        @centre = @event.centres.create!( @valid_attributes )
-        @centre.slug.should == "centre-slug"
+        centre = event.centres.create!( attributes )
+
+        expect(centre.slug).to eq(attributes[:slug])
       end
 
       it "can generate a unique slug where a centre with the same name exists" do
-        @centre_one = @event.centres.create!( @valid_attributes.merge({:slug => nil}) )
-        @centre_two = @event.centres.create!( @valid_attributes.merge({:slug => nil}) )
+        centre_one = event.centres.create!( attributes.merge(slug: nil) )
+        centre_two = event.centres.create!( attributes.merge(slug: nil) )
 
-        @centre_one.slug.should == "london"
-        @centre_two.slug.should == "london-2"
+        expect(centre_one.slug).to eq(attributes[:name].parameterize)
+        expect(centre_two.slug).to eq("#{attributes[:name].parameterize}-2")
       end
     end
 
     context "given invalid attributes" do
-      it "can't create an award category without a name" do
-        @centre = @event.centres.build(@valid_attributes.merge({ :name => nil }))
-        @centre.should_not be_valid
+      it "can't create a centre without a name" do
+        centre = event.centres.build(attributes.merge(name: nil))
+
+        expect(centre).to_not be_valid
+        expect(centre.errors).to have_key(:name)
       end
     end
   end
