b653c161ccf00a75d2a1ff2ce42589f4afc1ec1b
Jordan Hatch <jordan@jordanh.net>
2015-07-07 12:24:10 +0100

    Remove attr_accessible from Admin model

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,4 @@
 class Admin < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  attr_accessible :email, :password, :password_confirmation, :remember_me
 end
