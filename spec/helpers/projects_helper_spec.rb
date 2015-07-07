29f2a611f9ed558d0a484c6d713649f04492f1ee
Jordan Hatch <jordan@jordanh.net>
2015-07-07 12:44:25 +0100

    Update ProjectsHelper spec with new syntax

--- spec/helpers/projects_helper_spec.rb
+++ spec/helpers/projects_helper_spec.rb
@@ -4,23 +4,33 @@ RSpec.describe ProjectsHelper do
 
   describe "#format_github_url" do
     it "returns the username and repository when given a valid http url" do
-      helper.format_github_url("http://github.com/someuser/somerepo").should == "someuser/somerepo"
+      output = helper.format_github_url("http://github.com/someuser/somerepo")
+
+      expect(output).to eq("someuser/somerepo")
     end
 
     it "returns the username and repository when given a valid https url" do
-      helper.format_github_url("https://github.com/someuser/somerepo").should == "someuser/somerepo"
+      output = helper.format_github_url("https://github.com/someuser/somerepo")
+
+      expect(output).to eq("someuser/somerepo")
     end
 
     it "returns the full url when a url with another host is provided" do
-      helper.format_github_url("https://definitely-not-github.com/blah").should == "https://definitely-not-github.com/blah"
+      output = helper.format_github_url("https://definitely-not-github.com/blah")
+
+      expect(output).to eq("https://definitely-not-github.com/blah")
     end
 
     it "returns the full url when a url with another host is provided with two parts to the path" do
-      helper.format_github_url("https://definitely-not-github.com/someuser/somerepo").should == "https://definitely-not-github.com/someuser/somerepo"
+      output = helper.format_github_url("https://definitely-not-github.com/someuser/somerepo")
+
+      expect(output).to eq("https://definitely-not-github.com/someuser/somerepo")
     end
 
     it "returns the username when a url on github.com does not have a user and repo" do
-      helper.format_github_url("https://github.com/someuser").should == "someuser"
+      output = helper.format_github_url("https://github.com/someuser")
+
+      expect(output).to eq("someuser")
     end
   end
 
